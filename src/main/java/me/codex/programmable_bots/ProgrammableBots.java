ebc192c326be798de0efc0df5976ad8197be575e
BJTMastermind <bjtmastermind@gmail.com>
2023-01-18 20:16:50 -0500

    Added basic mod setup.

---
+++ src/main/java/me/codex/programmable_bots/ProgrammableBots.java
@@ -0,0 +1,19 @@
+package me.codex.programmable_bots;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProgrammableBots implements ModInitializer {
+	public static final String MODID = "programmable_bots";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}

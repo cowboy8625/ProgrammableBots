8446e700a5217bcef8b006496da6f1be8fb9c5a9
BJTMastermind <bjtmastermind@gmail.com>
2023-01-21 22:36:00 -0500

    Added GUI for the Bot Block

---
+++ src/main/java/me/codex/programmable_bots/screen/BotBlockScreenHandler.java
@@ -0,0 +1,97 @@
+package me.codex.programmable_bots.screen;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.ArrayPropertyDelegate;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+
+public class BotBlockScreenHandler extends ScreenHandler {
+    private final Inventory inventory;
+
+    public BotBlockScreenHandler(int syncId, PlayerInventory playerInventory) {
+        this(syncId, playerInventory, new SimpleInventory(22), new ArrayPropertyDelegate(0));
+    }
+
+    public BotBlockScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate delegate) {
+        super(ModScreenHandlers.BOT_SCREEN_HANDLER, syncId);
+        checkSize(inventory, 22);
+        this.inventory = inventory;
+        inventory.onOpen(playerInventory.player);
+
+        populateBotInventory();
+
+        addPlayerInventory(playerInventory);
+        addPlayerHotbar(playerInventory);
+
+        addProperties(delegate);
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int invSlot) {
+        ItemStack newStack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(invSlot);
+        if (slot != null && slot.hasStack()) {
+            ItemStack originalStack = slot.getStack();
+            newStack = originalStack.copy();
+            if (invSlot < this.inventory.size()) {
+                if (!this.insertItem(originalStack, this.inventory.size(), this.slots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (originalStack.isOf(Items.WRITABLE_BOOK)) {
+                if (!this.insertItem(originalStack, 0, 1, true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.insertItem(originalStack, 1, this.inventory.size(), false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (originalStack.isEmpty()) {
+                slot.setStack(ItemStack.EMPTY);
+            } else {
+                slot.markDirty();
+            }
+        }
+        return newStack;
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        return this.inventory.canPlayerUse(player);
+    }
+
+    private void populateBotInventory() {
+        this.addSlot(new Slot(inventory, 0, 8, 16) {
+            public boolean canInsert(ItemStack stack) {
+                return stack.isOf(Items.WRITABLE_BOOK);
+            }
+        });
+
+        int index = 1;
+        for (int r = 0; r < 7; r++) {
+            for (int c = 0; c < 3; c++) {
+                this.addSlot(new Slot(inventory, index, 44 + r * 18, 16 + c * 18));
+                index++;
+            }
+        }
+    }
+
+    private void addPlayerInventory(PlayerInventory inventory) {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 9; c++) {
+                this.addSlot(new Slot(inventory, c + r * 9 + 9, 8 + c * 18, 84 + r * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(PlayerInventory inventory) {
+        for (int i = 0; i < 9; i++) {
+            this.addSlot(new Slot(inventory, i, 8 + i * 18, 142));
+        }
+    }
+}
---
+++ src/main/java/me/codex/programmable_bots/screen/BotScreen.java
@@ -0,0 +1,43 @@
+package me.codex.programmable_bots.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import me.codex.programmable_bots.ProgrammableBots;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class BotScreen extends HandledScreen<BotBlockScreenHandler> {
+    private static final Identifier TEXTURE = new Identifier(ProgrammableBots.MODID, "textures/gui/container/bot.png");
+
+    public BotScreen(BotBlockScreenHandler handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
+    }
+
+    @Override
+    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        int x = (width - backgroundWidth) / 2;
+        int y = (height - backgroundHeight) / 2;
+        drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        renderBackground(matrices);
+        super.render(matrices, mouseX, mouseY, delta);
+        drawMouseoverTooltip(matrices, mouseX, mouseY);
+    }
+
+}
